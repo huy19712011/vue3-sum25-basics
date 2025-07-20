ebff051df681a69af7da39b2254db3f5d4a5eab8
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-20 20:35:29 +0700

    V3: List Rendering

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@ import { ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 
 const header = ref("Shopping List Application");
+
+const items = ref([
+  { id: 1, label: "10 product-1" },
+  { id: 2, label: "2 product-2" },
+  { id: 3, label: "3 product-3" },
+  { id: 4, label: "4 product-4" },
+]);
 </script>
 
 <template>
@@ -30,6 +37,11 @@ const header = ref("Shopping List Application");
   <h1>{{ message || "Welcome" }}</h1> -->
 
   <h1>{{ header }}</h1>
+
+  <ul>
+    <!-- <li v-for="{ id, label } in items" v-bind:key="id">{{ label }}</li> -->
+    <li v-for="({ id, label }, index) in items" v-bind:key="id">{{ index }}: {{ label }}</li>
+  </ul>
 
   <!-- <RouterView /> -->
 </template>
