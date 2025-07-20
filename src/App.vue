d7753a93a7b81789879e1f4862e92cf9cc1595b4
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-20 22:47:16 +0700

    V5: using event to add new item to list

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,7 @@ const items = ref([
 
 const newItem = ref("");
 
-const newItemPriority = ref("low");
-
 const newItemHighPriority = ref(false);
-
-const iceCreamFlavors = ref([]);
 </script>
 
 <template>
@@ -39,49 +35,20 @@ const iceCreamFlavors = ref([]);
     </nav>
   </header>
 
-  <!-- <h1>{{ message.toLocaleUpperCase() }}</h1>
-
-  <h1>{{ message ? message : "Welcome" }}</h1>
-  <h1>{{ message || "Welcome" }}</h1> -->
-
   <h1>{{ header }}</h1>
-  <input v-model.trim="newItem" type="text" placeholder="Add an Item" />
-  <!-- {{ newItem }} -->
+  <form
+    class="add-item-form"
+    v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })"
+  >
+    <input v-model.trim="newItem" type="text" placeholder="Add an Item" />
+    <!-- {{ newItem }} -->
 
-  <!-- <label>
-    Priority:
-    <input type="radio" v-model="newItemPriority" value="low" />
-    Low
-  </label>
-  <label>
-    <input type="radio" v-model="newItemPriority" value="high" />
-    High
-  </label> -->
-
-  <!-- <label>
-    Priority:
-    <select v-model="newItemPriority">
-      <option value="low">Low</option>
-      <option value="high">High</option>
-    </select>
-  </label>
-  <br />
-  {{ newItemPriority }} -->
-
-  <label>
-    <input type="checkbox" v-model="newItemHighPriority" />
-    High Priority
-  </label>
-  <br />
-  {{ newItemHighPriority }}
-
-  <br />
-  <label> <input type="checkbox" value="vanilla" v-model="iceCreamFlavors" /> Vanilla </label>
-  <label> <input type="checkbox" value="chocolate" v-model="iceCreamFlavors" /> Chocolate </label>
-  <label> <input type="checkbox" value="strawberry" v-model="iceCreamFlavors" /> Strawberry </label
-  ><br />
-  {{ iceCreamFlavors }}
-
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      High Priority
+    </label>
+    <button class="btn btn-primary">Save Item</button>
+  </form>
   <ul>
     <!-- <li v-for="{ id, label } in items" v-bind:key="id">{{ label }}</li> -->
     <li v-for="({ id, label }, index) in items" v-bind:key="id">{{ index }}: {{ label }}</li>
