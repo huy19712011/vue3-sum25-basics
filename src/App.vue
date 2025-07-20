c9a778441bc49aa3843924068f8ef4f7baf7403e
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-20 22:13:28 +0700

    V4: Input: input, radio, checkbox, select

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,14 @@ const items = ref([
   { id: 3, label: "3 product-3" },
   { id: 4, label: "4 product-4" },
 ]);
+
+const newItem = ref("");
+
+const newItemPriority = ref("low");
+
+const newItemHighPriority = ref(false);
+
+const iceCreamFlavors = ref([]);
 </script>
 
 <template>
@@ -37,6 +45,42 @@ const items = ref([
   <h1>{{ message || "Welcome" }}</h1> -->
 
   <h1>{{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Add an Item" />
+  <!-- {{ newItem }} -->
+
+  <!-- <label>
+    Priority:
+    <input type="radio" v-model="newItemPriority" value="low" />
+    Low
+  </label>
+  <label>
+    <input type="radio" v-model="newItemPriority" value="high" />
+    High
+  </label> -->
+
+  <!-- <label>
+    Priority:
+    <select v-model="newItemPriority">
+      <option value="low">Low</option>
+      <option value="high">High</option>
+    </select>
+  </label>
+  <br />
+  {{ newItemPriority }} -->
+
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority" />
+    High Priority
+  </label>
+  <br />
+  {{ newItemHighPriority }}
+
+  <br />
+  <label> <input type="checkbox" value="vanilla" v-model="iceCreamFlavors" /> Vanilla </label>
+  <label> <input type="checkbox" value="chocolate" v-model="iceCreamFlavors" /> Chocolate </label>
+  <label> <input type="checkbox" value="strawberry" v-model="iceCreamFlavors" /> Strawberry </label
+  ><br />
+  {{ iceCreamFlavors }}
 
   <ul>
     <!-- <li v-for="{ id, label } in items" v-bind:key="id">{{ label }}</li> -->
