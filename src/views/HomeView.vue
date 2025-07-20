909121d898313f5a9d897142e798722a78d82b56
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-20 16:01:13 +0700

    installing bootstrap; adding navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>This is a home page</h1>
+  </div>
 </template>
